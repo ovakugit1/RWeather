a44a55100acad648d55609208dfb03b36a93507f
ovakugit1 <rugocip@stexsy.com>
2016-09-26 15:05:55 +0300

    Modified about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,15 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to About page</p>
+      <h1 className="text-center">About</h1>
+      <p>This project was created to teach me ReactJS. You can find some resources below.</p>
+      <ul className="menu">
+        <li>
+          <a href="https://github.com/ovakugit1" target="_blank">GitHub</a>
+        </li>
+        <li>
+        </li>
+      </ul>
     </div>
   );
 };
